c628382216b9218b9851eeb983670206b191915e
Mansourkamar22-IT <mansourkamardine080@gmail.com>
2025-01-12 12:43:54 +0200

    Add files via upload

---
+++ ReaL-Estate-Website/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () =>{
+    return(
+        <>
+        <header>
+            <div className='container flex'>
+                <div className='logo'>
+                    </div>
+                    </div>
+                    </header>
+                    </>
+    )
+}
+export default Header
